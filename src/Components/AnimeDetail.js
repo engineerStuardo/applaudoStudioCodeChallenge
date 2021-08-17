d46ac4ef64fc6af3181e051148489e526bb3aebe
icortez <icortez@ufg.edu.sv>
2021-08-17 14:36:51 -0600

    make api request to id serie coming from AnimeScreen and then show it. Also addin goback and youtube button and finally show more or less in synopsis

--- src/Components/AnimeDetail.js
+++ src/Components/AnimeDetail.js
@@ -1,81 +1,164 @@
-import React from 'react';
-import {View, Text, Image, Dimensions, ScrollView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import styled from 'styled-components/native';
+
+import {ListLoader} from '../Components/ListLoader';
 
 const {width, height} = Dimensions.get('screen');
 
-export const AnimeDetail = ({route}) => {
-  const {anime} = route.params;
-  console.log(JSON.stringify(anime, null, 2));
+import {getAnime} from '../Services/Services';
+
+const GoBackButton = styled(TouchableOpacity)`
+  position: absolute;
+  top: 25px;
+  left: 25px;
+  z-index: 999;
+`;
+
+const YoutubeButton = styled(TouchableOpacity)`
+  position: absolute;
+  top: 270px;
+  left: 70px;
+  z-index: 99999;
+`;
+
+export const AnimeDetail = ({route, navigation}) => {
+  const {animeId} = route.params;
+  const [anime, setAnime] = useState();
+  const [loading, setLoading] = useState(false);
+  const [moreSynopsis, setMoreSynopsis] = useState(false);
+
+  const getAnimeDetail = async () => {
+    try {
+      setLoading(true);
+      const data = await getAnime(animeId);
+      console.log(JSON.stringify(data, null, 2));
+      setAnime(data);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAnimeDetail();
+  }, []);
 
   return (
     <ScrollView>
-      <Animatable.View
-        animation="slideInUp"
-        delay={400}
-        style={{position: 'absolute', top: 0, zIndex: 9}}>
-        <Image
-          resizeMode="cover"
-          style={{width: width, height: height / 1.5}}
-          source={{
-            uri: anime.item.attributes.posterImage.original,
-          }}
-        />
-      </Animatable.View>
-      <Animatable.View
-        animation="slideInDown"
-        delay={400}
-        style={{
-          zIndex: 10,
-          marginTop: height / 2.5,
-          borderTopLeftRadius: 50,
-          borderTopRightRadius: 50,
-          backgroundColor: 'white',
-        }}>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-          }}>
-          <Image
-            style={{width: 150, height: 250, margin: 15, borderRadius: 15}}
-            source={{
-              uri: anime.item.attributes.posterImage.original,
-            }}
-          />
-          <View
+      {loading && <ListLoader loading />}
+      {anime && (
+        <>
+          <GoBackButton onPress={() => navigation.goBack()}>
+            <Icon name={'chevron-left'} size={35} color={'orange'} />
+          </GoBackButton>
+          <Animatable.View
+            animation="slideInUp"
+            delay={400}
+            style={{position: 'absolute', top: 0, zIndex: 9}}>
+            <Image
+              resizeMode="cover"
+              style={{width: width, height: height / 1.5}}
+              source={{
+                uri: anime.attributes.posterImage.original,
+              }}
+            />
+          </Animatable.View>
+          <Animatable.View
+            animation="slideInDown"
+            delay={400}
             style={{
-              justifyContent: 'center',
-              // backgroundColor: 'red',
-              width: width / 2,
+              zIndex: 10,
+              marginTop: height / 2.5,
+              borderTopLeftRadius: 50,
+              borderTopRightRadius: 50,
+              backgroundColor: 'white',
+              elevation: 50,
             }}>
-            <Text style={{fontSize: 25, fontWeight: 'bold'}}>
-              {anime.item.attributes.titles.en ||
-                anime.item.attributes.titles.en_jp}
-            </Text>
-            <Text style={{fontSize: 15, fontWeight: 'bold'}}>
-              Popularity Rank:{' '}
-            </Text>
-            <Text>{anime.item.attributes.popularityRank || 'Not Found'}</Text>
-            <Text style={{fontSize: 15, fontWeight: 'bold'}}>Rating Rank:</Text>
-            <Text> {anime.item.attributes.ratingRank || 'Not Found'}</Text>
-            <Text style={{fontSize: 15, fontWeight: 'bold'}}>
-              Episode Count:
-            </Text>
-            <Text> {anime.item.attributes.episodeCount || 'Not Found'}</Text>
-            <Text style={{fontSize: 15, fontWeight: 'bold'}}>
-              Episode Length:
-            </Text>
-            <Text> {anime.item.attributes.episodeLength || 'Not Found'}</Text>
-          </View>
-        </View>
-        <View style={{padding: 25, alignItems: 'center'}}>
-          <Text style={{fontSize: 25, fontWeight: 'bold', marginBottom: 15}}>
-            Synopsis
-          </Text>
-          <Text>{anime.item.attributes.synopsis || 'Not Found'}</Text>
-        </View>
-      </Animatable.View>
+            <YoutubeButton
+              onPress={() => {
+                Linking.openURL(
+                  `vnd.youtube://watch/${anime.attributes.youtubeVideoId}`,
+                ).catch(err => {
+                  console.log(err);
+                });
+              }}>
+              <Icon name={'youtube-square'} size={40} color={'red'} />
+            </YoutubeButton>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+              }}>
+              <Image
+                style={{width: 150, height: 250, margin: 15, borderRadius: 15}}
+                source={{
+                  uri: anime.attributes.posterImage.original,
+                }}
+              />
+              <View
+                style={{
+                  justifyContent: 'center',
+                  // backgroundColor: 'red',
+                  width: width / 2,
+                }}>
+                <Text style={{fontSize: 25, fontWeight: 'bold'}}>
+                  {anime.attributes.titles.en || anime.attributes.titles.en_jp}
+                </Text>
+                <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                  Popularity Rank:{' '}
+                </Text>
+                <Text>{anime.attributes.popularityRank || 'Not Found'}</Text>
+                <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                  Rating Rank:
+                </Text>
+                <Text> {anime.attributes.ratingRank || 'Not Found'}</Text>
+                <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                  Episode Count:
+                </Text>
+                <Text> {anime.attributes.episodeCount || 'Not Found'}</Text>
+                <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                  Episode Length:
+                </Text>
+                <Text> {anime.attributes.episodeLength || 'Not Found'}</Text>
+              </View>
+            </View>
+            <View style={{padding: 25, alignItems: 'center'}}>
+              <Text
+                style={{
+                  fontSize: 25,
+                  fontWeight: 'bold',
+                  marginBottom: 15,
+                  marginTop: 20,
+                }}>
+                Synopsis
+              </Text>
+              <Text>
+                {!moreSynopsis
+                  ? `${anime.attributes.synopsis.substring(0, 200)}...`
+                  : anime.attributes.synopsis || 'Not Found'}
+              </Text>
+              <TouchableOpacity onPress={() => setMoreSynopsis(!moreSynopsis)}>
+                <Text style={{marginTop: 10, color: 'orange'}}>
+                  {!moreSynopsis ? 'Show More +' : 'Show Less -'}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Animatable.View>
+        </>
+      )}
     </ScrollView>
   );
 };
