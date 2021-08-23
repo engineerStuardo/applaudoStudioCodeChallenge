c0f725c7d8a4cba3d271ddf8a4712acd996ef8e1
icortez <icortez@ufg.edu.sv>
2021-08-23 11:12:57 -0600

    fixing some style width in ListImagesStyles

--- src/Styles/ListImagesStyles.js
+++ src/Styles/ListImagesStyles.js
@@ -5,6 +5,10 @@ const ListImageContainer = styled(View)`
   flex: 1;
   width: 360px;
   align-items: center;
+  margin-left: ${({isPortrait, orientation}) =>
+    isPortrait && orientation.width > 360
+      ? `${orientation.width / 18}px`
+      : '0px'};
   margin-top: ${({isPortrait, orientation}) =>
     isPortrait || orientation.width <= 780 ? '0px' : '20px'};
 `;
