270c35d53996c722fb62d8d7ddd3ae45fc8ca360
engineerStuardo <ingeniero.stuardo@gmail.com>
2021-09-03 04:27:37 -0600

    strat testing checkInternet functionality

--- src/Screens/AnimeScreen.js
+++ src/Screens/AnimeScreen.js
@@ -1,17 +1,26 @@
-import React, {useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import {useFocusEffect} from '@react-navigation/native';
+import NetInfo from '@react-native-community/netinfo';
 
 import {Anime} from '../Components/Anime';
 import {useTypeContext} from '../CustomHooks/useTypeContext';
+import {NoConnection} from '../Components/NoConnection';
 
 export const AnimeScreen = () => {
+  const [internet, setInternet] = useState();
   const {setType} = useTypeContext();
+
+  const checkInternet = async () => {
+    const state = await NetInfo.fetch();
+    !state.isConnected ? setInternet(false) : setInternet(true);
+  };
 
   useFocusEffect(
     useCallback(() => {
       setType('anime');
+      checkInternet();
     }, []),
   );
 
-  return <Anime />;
+  return internet ? <Anime /> : <NoConnection checkInternet={checkInternet} />;
 };
