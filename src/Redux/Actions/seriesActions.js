36dbe1049ae555f5798e3f8c36e868e18a5dcdae
icortez <icortez@ufg.edu.sv>
2021-09-04 12:03:44 -0600

    start working in api request

--- src/Redux/Actions/seriesActions.js
+++ src/Redux/Actions/seriesActions.js
@@ -1,6 +1,42 @@
 import {SeriesActionsTypes} from '../Types/seriesTypes';
+import {getFullData, getSearch} from '../../Services/Services';
 
 export const addType = type => ({
   type: SeriesActionsTypes.ADD_TYPE,
   payload: type,
+});
+
+export const getData = (type, start, offset) => {
+  return async dispatch => {
+    dispatch(searchStarted());
+    try {
+      if (start) {
+        const data = await getFullData(type, 0);
+        dispatch(searchSuccess(data));
+      } else {
+        const data = await getFullData(type, offset);
+        dispatch(searchSuccess(data));
+      }
+    } catch (error) {
+      dispatch(searchFailure(error));
+    }
+  };
+};
+
+const searchSuccess = data => ({
+  type: SeriesActionsTypes.SEARCH_SUCCESS,
+  payload: {
+    ...data,
+  },
+});
+
+const searchStarted = () => ({
+  type: SeriesActionsTypes.SEARCH_STARTED,
+});
+
+const searchFailure = error => ({
+  type: SeriesActionsTypes.SEARCH_FAILURE,
+  payload: {
+    error,
+  },
 });
